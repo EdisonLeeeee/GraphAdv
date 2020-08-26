1ade2f25c9cb6e9ca5d96ffa76471245f1b00362
EdisonLeeeee <cnljt@outlook.com>
2020-08-26 08:53:12 -0400

    0.1.0

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 from graphadv import __version__
 
+VERSION = __version__
 url = 'https://github.com/EdisonLeeeee/GraphgAdv'
 
 install_requires = [
@@ -20,14 +21,14 @@ install_requires = [
 
 setup(
     name='graphadv',
-    version=__version__,
+    version=VERSION,
     description='Geometric Adversarial Learning Library for TensorFlow',
     author='Jintang Li',
     author_email='cnljt@outlook.com',
     long_description=open("README.md", encoding="utf-8").read(),
     long_description_content_type="text/markdown",
     url=url,
-    download_url='{}/archive/{}.tar.gz'.format(url, __version__),
+    download_url='{}/archive/{}.tar.gz'.format(url, VERSION),
     keywords=[
         'tensorflow',
         'geometric-deep-learning',
@@ -36,6 +37,15 @@ setup(
         'graph-robustness'
     ],
     python_requires='>=3.6',
+    license="MIT LICENSE",    
     install_requires=install_requires,
-    packages=find_packages()
+    packages=find_packages(exclude=("examples", "imgs")),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Scientific/Engineering :: Artificial Intelligence",
+    ],
 )
+
